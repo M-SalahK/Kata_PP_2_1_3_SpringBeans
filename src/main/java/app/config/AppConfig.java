3eca397a57ddcccbdd9e06022d14deb4550c8055
magom <magomed.kibiev@gmail.com>
2024-08-10 05:20:54 +0300

    Ну таймер меня немного разнес конечно

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,7 +1,10 @@
 package app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "app")
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,17 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("timerMane")
+
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
-
+    @Autowired
     public Long getTime() {
         return nanoTime;
     }
